f3981feadda1dd5d10f651b406ddeb158b522805
b00tsn0w <b00tsn0w@outlook.fr>
2015-12-13 15:55:39 +0100

    add website

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+
+	<link rel="icon" href="img/icon.ico" />
+	<link rel="stylesheet" href="base/base.css">
+	<link rel="stylesheet" href="base/style.css">
+
+	<title>FirmTool</title>
+</head>
+<body>
+	
+	<div class="topbar">
+		<h1>FirmTool</h1>
+	</div>
+
+	<div class="video">
+		<iframe width="600" height="338" src="https://www.youtube.com/embed/dZJfrvx9Lwc" frameborder="0" allowfullscreen></iframe>
+	</div>
+
+	<div class="dl">
+		<a href="https://raw.githubusercontent.com/b00tsn0w/FirmTool/gh-pages/exe/FirmTool.exe">Download here</a>
+	</div>
+
+	<div class="log">
+		<h2>Change</h2>
+		<h3>v1.0</h3>
+		<p>- First release</p>
+	</div>
+
+</body>
+</html>
