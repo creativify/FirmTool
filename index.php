4fe0b666da8c8a3115a594387763a5824f4aa737
b00tsn0w <b00tsn0w@outlook.fr>
2016-01-13 19:27:35 +0100

    Add Conf iOS 9.X Dev

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+
+	<link rel="icon" href="img/icon.ico" />
+	<link rel="stylesheet" href="base/base.css">
+	<link rel="stylesheet" href="base/style.css">
+
+	<title>FirmTool</title>
+</head>
+<body>
+	
+	<div class="topbar">
+		<h1>FirmTool</h1>
+	</div>
+
+	<div class="exp">
+		 FirmTool is software that allows you to download your IPSW !
+	</div>
+
+	<div class="video">
+		<iframe width="600" height="338" src="https://www.youtube.com/embed/dZJfrvx9Lwc" frameborder="0" allowfullscreen></iframe>
+	</div>
+
+	<div class="dl">
+		<a href="https://raw.githubusercontent.com/b00tsn0w/FirmTool/gh-pages/exe/FirmTool.exe">Download here</a>
+	</div>
+
+	<div class="log">
+		<h2>Change</h2>
+		<h3>v1.0</h3>
+		<p>- First release</p>
+	</div>
+
+	<div class="iosbeta">
+		<h2>iOS 9.3 Beta OTA Update</h2>
+		<p>iOS : <a href="conf_9.X/iOS.mobileconfig">Link Here</a></p>
+		<p>tvOS : <a href="conf_9.X/tvOS.mobileconfig">Link Here</a></p>
+		<p>WatchOS : <a href="conf_9.X/WatchOS.mobileconfig">Link Here</a></p>
+	</div>
+
+	<div class="iosbeta">
+		<h2>iOS 9.3 Beta IPSW Link</h2>
+		<p>Soon</p>
+	</div>
+
+</body>
+</html>
